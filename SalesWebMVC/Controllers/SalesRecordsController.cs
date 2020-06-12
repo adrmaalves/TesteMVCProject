51fb47019490c8971c5607706b2009896687d515
Adriano Alves <adrianomacedo_07@hotmail.com>
2020-06-12 15:31:14 +0100

    Implement Simple Search

--- SalesWebMVC/Controllers/SalesRecordsController.cs
+++ SalesWebMVC/Controllers/SalesRecordsController.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
 {
 	public class SalesRecordsController : Controller
 	{
+		private readonly SalesRecordService _salesrecordservice;
+
+		public SalesRecordsController(SalesRecordService salesrecordservice)
+		{
+			_salesrecordservice = salesrecordservice;
+		}
+
 		public IActionResult Index()
 		{
 			return View();
 		}
 
-		public IActionResult SimpleSearch()
+		public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
 		{
-			return View();
+			if (!minDate.HasValue)
+				minDate = new DateTime(DateTime.Now.Year, 1, 1);
+			if (!maxDate.HasValue)
+				maxDate = DateTime.Now;
+
+			ViewData["minDate"] = minDate.Value.ToString("MM/dd/yyyy");
+			ViewData["maxDate"] = maxDate.Value.ToString("dd/MM/yyyy");
+			var result = await _salesrecordservice.FindByDateAsync(minDate, maxDate);
+			return View(result);
 		}
 
 		public IActionResult GroupingSearch()
--- SalesWebMVC/Models/SalesRecord.cs
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace SalesWebMVC.Models
 {
 	public class SalesRecord
 	{
 		public int Id { get; set; }
+
+		[Required(ErrorMessage = "{0} required")]
+		[DataType(DataType.Date)]
+		[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
 		public DateTime Date { get; set; }
+
+		[Required(ErrorMessage ="Ammount has to be provided.")]
+		[Range(5.00, 999000000.00, ErrorMessage = "{0} must be from {1} to {2}")]
 		public double Amount { get; set; }
 		public SaleStatus Status { get; set; }
 		public Seller Seller { get; set; }
